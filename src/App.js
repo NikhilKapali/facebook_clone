4c88a3bdc1e056d26c275b0188723e14d1b86366
NikhilKapali <nikhilkapali12@gmail.com>
2021-01-15 13:48:48 +0545

    Story portion done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from './header'
-import Sidebar from './Sidebar'
-import Feed from './Feed'
+import Header from './header/header'
+import Sidebar from './sidebar/Sidebar'
+import Feed from './feed/Feed'
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <div className="app__body">
         <Sidebar />  
---
+++ src/feed/StoryReel.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Storyreel.css'
+import Story from "./Story"
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story
+                image="./nezuko.png"
+                profileSrc="./Facebook-logo.png"
+                title="Nezuko chwannn"
+            />
+            <Story
+                image="./nezuko.png"
+                profileSrc="./Facebook-logo.png"
+                title="Nezuko chwannn"
+            />
+            <Story
+                image="./nezuko.png"
+                profileSrc="./Facebook-logo.png"
+                title="Nezuko chwannn"
+            />
+            <Story
+                image="./nezuko.png"
+                profileSrc="./Facebook-logo.png"
+                title="Nezuko chwannn"
+            />
+            <Story
+                image="./nezuko.png"
+                profileSrc="./Facebook-logo.png"
+                title="Nezuko chwannn"
+            />
+            <Story
+                image="./nezuko.png"
+                profileSrc="./Facebook-logo.png"
+                title="Nezuko chwannn"
+            />
+        </div>
+    )
+}
+
+export default StoryReel
